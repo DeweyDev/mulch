df394dca3f02154b8293dc252d924f49df9d1a8f
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-05-22 21:19:07 -0400

    I did some ghetto thing that half works

--- mulch/engine.h
+++ mulch/engine.h
@@ -1,7 +1,28 @@
 #pragma once
+#include "std_lib_facilities.h"
 class engine
 {
 public:
+
+	// Public variables that will hold shit
+
+	typedef struct {
+
+		vector <string> elementNames;
+		vector <string> amountOfElement;
+
+
+
+	} databank;
+
+	// Instance of the database
+
+	databank DATA_BASE;
+
+	// Member functions
+
+	void parseInput(string stringInput);
+
 	engine(void);
 	~engine(void);
 };
