04f6449c057f32b8661d507c525c2f8ebc1ed8fc
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-05-22 21:20:48 -0400

    Revert "I did some ghetto thing that half works"
    This reverts commit df394dca3f02154b8293dc252d924f49df9d1a8f.

--- mulch/engine.cpp
+++ mulch/engine.cpp
@@ -1,5 +1,4 @@
 #include "engine.h"
-#include <ctype.h>
 
 
 engine::engine(void)
@@ -9,55 +8,4 @@ engine::engine(void)
 
 engine::~engine(void)
 {
-}
-
-void engine::parseInput(string input){
-
-	int i;
-	string temp;
-	vector <string> foundElements;
-
-	//scan through teh entire string
-
-	for(i = 0; i < input.size(); i++){
-
-		// scan each character if it is a letter
-
-		if(isalpha(input[i]) && isupper(input[i])){
-
-			// is it a one letter element or a two letter element?
-
-			if(isalpha(input[i + 1]) && islower(input[i + 1])){
-
-				// We have a two letter element and shove it in
-				temp.append(1,input[i]);
-				temp.append(1,input[i + 1]);
-
-				cout << "\nShoved in: " << temp << "\n";
-
-				foundElements.push_back(temp);
-
-				i++;
-				temp.clear();
-
-			}else if((isalpha(input[i + 1]) && isupper(input[i + 1])) || isdigit(input[i + 1]) || isspace(input[i + 1])){
-
-				// we have a one letter element and shove it in
-				temp.append(1,input[i]);
-
-				cout << "\nShoved in: " << temp << "\n";
-				foundElements.push_back(temp);
-
-				temp.clear();
-
-			}
-
-
-
-
-		}
-
-
-	}
-
 }
--- mulch/engine.h
+++ mulch/engine.h
@@ -1,28 +1,7 @@
 #pragma once
-#include "std_lib_facilities.h"
 class engine
 {
 public:
-
-	// Public variables that will hold shit
-
-	typedef struct {
-
-		vector <string> elementNames;
-		vector <string> amountOfElement;
-
-
-
-	} databank;
-
-	// Instance of the database
-
-	databank DATA_BASE;
-
-	// Member functions
-
-	void parseInput(string stringInput);
-
 	engine(void);
 	~engine(void);
 };
