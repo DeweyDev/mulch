362d1c9dc56c4758b7d22136bc691fe261c65afa
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-05-22 21:21:22 -0400

    Revert "Revert "I did some ghetto thing that half works""
    This reverts commit 04f6449c057f32b8661d507c525c2f8ebc1ed8fc.

--- mulch/main.cpp
+++ mulch/main.cpp
@@ -2,14 +2,19 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include "engine.h"
 
 // Main function
 
 int main( void ){
+	
+	engine newone;
+	string input;
 
+	printf("%s\n","Welcome to Mulch. Enter a balanced chemical equation below");
+	cin >> input;
 
-
-
+	newone.parseInput(input);
 
 	keep_window_open();
 	return 0;
